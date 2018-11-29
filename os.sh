393070f8a6a3c7d89b49d7f4920a39018124ae0f
Nathan Rajlich <nathan@tootallnate.net>
2018-11-28 23:28:48 -0800

    Fix

--- os.sh
+++ os.sh
@@ -1,10 +1,10 @@
 os_platform() {
   local platform
-  platform=$(uname -o | tr '[:upper:]' '[:lower:]')
+  platform="$(uname -o | tr '[:upper:]' '[:lower:]')"
 
   if [ "$platform" = "gnu/linux" ]; then
     platform="linux"
-  else
+  fi
 
   echo "$platform"
 }
