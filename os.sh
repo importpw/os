9c491493c8b41ef5b28c6684e869da8a4c929ffd
Nathan Rajlich <nathan@tootallnate.net>
2018-12-14 17:26:01 -0800

    Add `os_path_push` and `os_path_unshift`

--- os.sh
+++ os.sh
@@ -5,5 +5,35 @@ os_platform() {
 }
 
 os_arch() {
-  uname -m
+  local arch
+  arch="$(uname -m)"
+  echo "$arch"
+}
+
+os_path_push() {
+  local dir="$1"
+  local IFS=:
+  local sep=
+  local path=
+  for p in $PATH; do
+    if [ "$p" != "$dir" ]; then
+      path="${path}${sep}${p}"
+      sep=:
+    fi
+  done
+  export PATH="${path}${sep}${dir}"
+}
+
+os_path_unshift() {
+  local dir="$1"
+  local IFS=:
+  local sep=
+  local path=
+  for p in $PATH; do
+    if [ "$p" != "$dir" ]; then
+      path="${path}${sep}${p}"
+      sep=:
+    fi
+  done
+  export PATH="${dir}${sep}${path}"
 }
